265af39d6c2f036779e14f3b11357a1cf87c087d
stanislav <stychinskiisv@gmail.com>
2026-03-03 20:00:45 +0300

    WIP: add swagger/cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { GlobalModule } from './global.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(GlobalModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Template')
+        .setDescription('Template API')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    const methods = ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE', 'PATCH'];
+    const urls = ['http://localhost:3000'];
+    app.enableCors({
+        origin: urls,
+        methods: methods,
+        credentials: true,
+    });
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
