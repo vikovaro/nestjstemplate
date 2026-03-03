ca1f7c3a7ddada7adaecd068748cff534a4a1b15
stanislav <stychinskiisv@gmail.com>
2026-03-03 20:14:49 +0300

    WIP: add roles decorator

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,10 +26,14 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-        '@typescript-eslint/no-unsafe-call': 'off'
+      'comma-dangle': ['error', 'always-multiline'],
+      semi: ['error', 'always'],
+      'eol-last': ['error', 'always'],
+      indent: 'off',
     },
   },
 );
