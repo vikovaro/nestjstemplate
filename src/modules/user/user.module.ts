21e94b9d7882b46447f8e8906084cb69741374ac
stanislav <stychinskiisv@gmail.com>
2026-03-05 20:52:20 +0300

    Chore: add controller

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { UserController } from './user.controller';
 
 @Module({
-    imports: [],
+    controllers: [UserController],
     providers: [UserRepository, UserService],
     exports: [UserService, UserRepository],
 })
