899375d9f6c31fd8a5bc5a289705c1e26a32e48c
admin <stychinskiisv@gmail.com>
2026-03-05 17:03:47 +0300

    Chore: fix add user

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { User } from './models/user.model';
 import { PrismaService } from '../../database/prisma/prisma.service';
 import { AuthUser } from './models/auth.user.model';
+import { ERole } from '../../common/enums/role.enum';
 
 @Injectable()
 export class UserRepository {
     constructor(private readonly prisma: PrismaService) {}
 
     async addUser(user: AuthUser): Promise<User> {
-        return this.prisma.user.create({ data: user });
+        return this.prisma.user.create({ data: { ...user, role: ERole.User }});
     }
 
     async getUser(userId: string): Promise<User | null> {
