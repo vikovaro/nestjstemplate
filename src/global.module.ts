e6f0511805301d6d690063a72039726fe61bc0e8
admin <stychinskiisv@gmail.com>
2026-03-05 17:00:04 +0300

    Chore: add jwt

--- src/global.module.ts
+++ src/global.module.ts
@@ -1,12 +1,24 @@
 import { Module, Global } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaService } from './database/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
+        }),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: {
+                    expiresIn: '1h',
+                },
+                global: true,
+            }),
         }),
     ],
     providers: [PrismaService],
