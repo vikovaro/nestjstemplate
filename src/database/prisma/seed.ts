7f5db776e0705aea273a2d60edd2407897ac71a4
stanislav <stychinskiisv@gmail.com>
2026-03-05 19:50:41 +0300

    Chore: seed prisma

---
+++ src/database/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import * as argon2 from 'argon2';
+import { ERole } from '../../common/enums/role.enum';
+const prisma = new PrismaClient();
+
+async function main() {
+    const hashedPassword = await argon2.hash('admin');
+    await prisma.user.create({
+        data: {
+            username: 'admin',
+            password: hashedPassword,
+            email: '1@gmail.com',
+            phone: '+71112223344',
+            role: ERole.Admin,
+        }
+    });
+
+    console.log('Seed data created successfully!');
+}
+
+main()
+    .catch((e) => {
+        console.error('Seeding failed:', e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
