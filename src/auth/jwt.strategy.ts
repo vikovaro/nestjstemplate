67b766d87fa0474f5190b86e94123d173fed6869
admin <stychinskiisv@gmail.com>
2026-03-05 17:00:36 +0300

    Chore: add jwt

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { TRole } from '../common/types/role.type';
+
+export interface JwtPayload {
+    userId: string;
+    role: TRole;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly configService: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET'),
+        });
+    }
+
+    async validate(payload: JwtPayload) {
+        return {
+            userId: payload.userId,
+        };
+    }
+}
