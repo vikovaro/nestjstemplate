77e4facd28e97eb03a494608c505acecdd5ed739
admin <stychinskiisv@gmail.com>
2026-03-05 17:00:27 +0300

    WIP: get me request

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
 @Controller('user')
 @ApiTags('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/getMe')
+    getProfile(@Req() req) {
+        return req.user;
+    }
 }
