adc720592970fb6c577a0e64a06d69bfb7e84b3f
admin <stychinskiisv@gmail.com>
2026-03-05 16:16:33 +0300

    WIP: add addUser method

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './models/user.model';
 import { PrismaService } from '../../database/prisma/prisma.service';
+import { AuthUser } from './models/auth.user.model';
 
 @Injectable()
 export class UserRepository {
     constructor(private readonly prisma: PrismaService) {}
 
-    async getUser(userId: number) {
-        return await this.prisma.user.findUnique({ id: userId });
+    async addUser(user: AuthUser): Promise<User> {
+        return this.prisma.user.create({ data: user });
     }
 
-    async getUserByUsername(username: string) {
-        return await this.prisma.user.findUnique({ username: username });
+    async getUser(userId: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { id: userId },
+        });
+    }
+
+    async getAuthUser(username: string): Promise<AuthUser | null> {
+        return this.prisma.user.findUnique({
+            where: { username },
+        });
+    }
+
+    async getUserByUsername(username: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { username },
+        });
     }
 }
