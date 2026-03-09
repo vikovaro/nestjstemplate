63edd7c2d8ed1c485cff69ca859f043cc3d2a84b
admin <stychinskiisv@gmail.com>
2026-03-09 21:52:07 +0300

    Chore: add refresh guard

---
+++ src/auth/guards/auth-refresh.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthRefreshRestGuard implements CanActivate {
+    constructor(private jwtService: JwtService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+        if (!token) {
+            throw new UnauthorizedException();
+        }
+        try {
+            const data = await this.jwtService.verifyAsync(token);
+
+            if (data.type !== 'Refresh') {
+                throw new UnauthorizedException();
+            }
+
+            request['data'] = {
+                ...data,
+                token,
+            };
+        } catch {
+            throw new UnauthorizedException();
+        }
+        return true;
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
+}
